584be5ad37fbf8cf4db88aa5728c3c6984c0020c
keitaro miyano <miyakeionigiri@yahoo.co.jp>
2020-09-22 13:27:07 +0800

    Initial Commit

---
+++ cat_dog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cat_dog
+//
+//  Created by 宮野奎太朗 on 2020/09/22.
+//  Copyright © 2020 keitaro miyano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ cat_dogTests/cat_dogTests.swift
@@ -0,0 +1,34 @@
+//
+//  cat_dogTests.swift
+//  cat_dogTests
+//
+//  Created by 宮野奎太朗 on 2020/09/22.
+//  Copyright © 2020 keitaro miyano. All rights reserved.
+//
+
+import XCTest
+@testable import cat_dog
+
+class cat_dogTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
